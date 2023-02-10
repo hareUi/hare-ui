8ca0b14d9dfe65d527ca15b387f6f9967ae85509
dora <2378643343@qq.com>
2023-02-10 19:57:43 +0800

    完善抽屉板组件。

--- docs/demos/drawer/drawer-2.vue
+++ docs/demos/drawer/drawer-2.vue
@@ -1,19 +1,14 @@
 <template>
   <h-button variant="solid" color="primary" @click="showDrawer">Click Me</h-button>
-  <h-drawer v-model="visible" position="left" style="padding: 20px;">Left Drawer</h-drawer>
+  <h-drawer v-model="visible" style="padding: 20px;">Hello Drawer</h-drawer>
 </template>
 <script>
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   setup() {
-    const visible = ref(true)
-    const showDrawer = () => {
-      // console.log(visible)
-      visible.value = true
-      // console.log(visible)
-      // return visible.value
-    }
+    const visible = ref(false)
+    const showDrawer = () => (visible.value = true)
 
     return { visible, showDrawer }
   }
