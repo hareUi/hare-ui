688386c3044c4d950a439c189f6675a1d2af0c28
TFX <1814618324@qq.com>
2023-02-16 18:12:17 +0800

    change

--- docs/demos/icon/icon-1.vue
+++ docs/demos/icon/icon-1.vue
@@ -1,7 +1,7 @@
 <template>
   <h-icon name="vue" :size="40"></h-icon>
   <h-icon name="home" size="40px"></h-icon>
-  <h-icon name="user" color="#475313"></h-icon>
+  <h-icon name="user" color="#475313">col</h-icon>
 </template>
 
 <script lang="ts" setup></script>
