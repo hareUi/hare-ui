bb3a53eea11448a0e8e0595854b1e57c554a7473
hotaru <907873499@qq.com>
2023-02-20 12:08:51 +0800

    文档&backtop 没写明白监听暂时只能一直出现QAQ

--- packages/back-to-top/src/back-to-top.tsx
+++ packages/back-to-top/src/back-to-top.tsx
@@ -1,4 +1,4 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, watch } from 'vue'
 import { backToTopProps } from './types'
 
 const NAME = 'h-back-to-top'
@@ -7,9 +7,37 @@ export default defineComponent({
   name: NAME,
   props: backToTopProps,
   setup(props, { slots }) {
+    // 获取页面偏移量
+
+    //  let a=document.getElementById('myBack')
+    let scrolly = window.pageYOffset
+    let a = computed(() => {
+      console.log(document.body.scrollTop)
+      if (scrolly > props.visiableHeight) {
+        console.log('a')
+        // a.style.display = 'absolute'
+      } else {
+        console.log('b')
+        // a.style.display = 'none'
+      }
+    })
+    const TestClick = () => {
+      console.log('aaaa')
+      document.body.scrollTop = document.documentElement.scrollTop = 0
+    }
+
     return () => (
-      <div class={NAME}>
-        <div>h-back-to-top</div>
+      <div
+        id="myBack"
+        class={NAME}
+        onClick={TestClick}
+        style={[`right:${props.right}` + 'px', `bottom:${props.bottom}` + 'px']}
+      >
+        <div class="inner">
+          <a href="#">
+            <img src="/back.png" alt="" />
+          </a>
+        </div>
       </div>
     )
   }
