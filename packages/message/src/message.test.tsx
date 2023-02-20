68b1ecc00ebc39b5dcbc4edc9e6b8c10e0eed309
啊这！ <oncwnuISEQ3CquMPjbBKsppssthI@git.weixin.qq.com>
2023-02-20 20:50:38 +0800

    fix(card、message): message、card测试样例不符合规范，对其进行删除

--- packages/message/src/message.test.tsx
+++ packages/message/src/message.test.tsx
@@ -1,3 +1,7 @@
 import Message from './message'
 import { mount } from '@vue/test-utils'
-describe('----测试message组件----', () => {})
+describe('----测试message组件----', () => {
+  it('message组件测试', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
