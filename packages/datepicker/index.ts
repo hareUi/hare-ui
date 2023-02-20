7a755f9ecd220cb9c90e4586ac36f8e231fbe91f
Zymonody7 <111135613+Zymonody7@users.noreply.github.com>
2023-02-20 20:22:20 +0800

    Delete packages/datepicker directory

--- packages/datepicker/index.ts
+++
@@ -1,9 +0,0 @@
-import Datepicker from './src/datepicker'
-import { App } from 'vue'
-
-Datepicker.install = (app: App): void => {
-  // 注册组件
-  app.component(Datepicker.name, Datepicker)
-}
-
-export default Datepicker
--- packages/datepicker/src/mode/week.tsx
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'weekMode',
-  setup(props, ctx) {
-    return () => {
-      return <div>week</div>
-    }
-  }
-})
